1c4ccd4932f7a01c3c1451643f032caf03d98ea2
Bushra Bonkiye <bushrabonkiye@gmail.com>
2022-07-16 15:12:17 -0400

    Major site redesign

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let isMenuActive = false;
 
 document.getElementById("techdoc_proj").onclick = function () {
     location.href = "/Projects/FCCTechnicalDocumentation/main.html"
@@ -17,4 +18,13 @@ document.getElementById("quote_generator_proj").onclick = function() {
 
 document.getElementById("top_level_proj").onclick = function() {
     location.href = "Projects/TopLevelLockAndKey/index.html"
+}
+
+function handleMenuClick() {
+    if(isMenuActive) {
+        document.getElementById("navbar").style.display = "none"
+    } else {
+        document.getElementById("navbar").style.display = "flex"
+    }
+    isMenuActive =!isMenuActive;
 }
