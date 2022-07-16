336c0cdb5744fa254c4a5981d271ff3c50713229
Bushra Bonkiye <bushrabonkiye@gmail.com>
2022-07-16 19:19:06 -0400

    Removed unnecessary javascript code

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ document.getElementById("pomodoro_proj").onclick = function () {
     location.href = "/Projects/Pomodoro/main.html"
 }
 
-document.getElementById("todo_list_proj").onclick = function () {
-    location.href = "/Projects/TodoList/main.html"
-}
-
-document.getElementById("quote_generator_proj").onclick = function() {
-    location.href = "Projects/QuoteGenerator/main.html"
-}
-
-document.getElementById("top_level_proj").onclick = function() {
-    location.href = "Projects/TopLevelLockAndKey/index.html"
-}
-
 function handleMenuClick() {
     if(isMenuActive) {
         document.getElementById("navbar").style.display = "none"
