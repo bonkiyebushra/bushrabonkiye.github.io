640d77edae6ab8663e3dc044964859dc1e74333e
Bushra Bonkiye <bushrabonkiye@gmail.com>
2024-01-13 09:21:19 -0500

    Links to other websites open in new tab

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 let isMenuActive = false;
 
-document.getElementById("computer_repair_guy").onclick = function () {
-    location.href = "/Projects/computer_repair_guy/index.html"
-}
-
-document.getElementById("merve_care_proj").onclick = function () {
-    location.href = "https://mervecare.com"
-}
-
 function handleMenuClick() {
     if(isMenuActive) {
         document.getElementById("navbar").style.display = "none"
